23e1bad99f7712f1d2e40a7170bbe7214ed4c88c
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2020-08-25 16:59:16 +0100

    add more js
    add js to go to booking page when submit button is clicked.

--- js/apps.js
+++ js/apps.js
@@ -1,26 +1,49 @@
-// want a function to highlight the image if user clicks it
+
 document.addEventListener("DOMContentLoaded", function() {
+
+    // created a function to highlight the image if user clicks it... 
+
     var hamilton = document.getElementById('hamilton');
     var lionKing = document.getElementById('lionKing');
     var bookOfMormon = document.getElementById('bookOfMormon');
     var wicked = document.getElementById('wicked');
+
+    // hamilton.addEventListener("click", function (event) {
     
-    hamilton.addEventListener("click", function (event) {
-    
-        // when click image - highlight pic and select this play in dropdown
-        event.target.style.backgroundImage = "url(pics/hamilton2.jpg)";
+    //     // when click image - highlight pic and select this play in dropdown
+    //     event.target.style.backgroundImage = "url(pics/hamilton2.jpg)";
+    // });
+
+
+
+    // tried creating a function that highlights an image if it's selected in the dropdown
+        
+    var play = document.getElementById('play');
+    play.addEventListener('change', function (event) {
+        var ddValue = event.target.value;
+
+        switch (ddValue) {
+            case hamilton.id: 
+            hamilton.style.backgroundImage = "url(pics/hamilton2.jpg)";
+            break;
+
+            case lionKing.id:
+            lionKing.style.backgroundImage = "url(pics/lionKing.jpg)";
+            break;
+
+            case bookOfMormon.id: 
+            bookOfMormon.style.backgroundImage = "url(pics/bookOfMormon.jpg)";
+            break;
+
+            case wicked.id: 
+            wicked.style.backgroundImage = "url(pics/wicked.jpg)";
+            break;
+        }
+
     });
+
 });
 
-
-
-// want another function to highlight the image if user picks that play in dropdown
-
-
-// did a little JS code test here.. will delete this before merging with master branch
-
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     var listItem = document.createElement('li');
-    //     listItem.innerText = 'Testing - I was created in JS!';
-    //     document.getElementById("jsTest").appendChild(listItem);
-    // });
+function goToBooking() {
+    window.location="pages/booking.html";
+};
